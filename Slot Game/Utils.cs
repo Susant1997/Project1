8a419cdf1ac746b4bd9cf241754badf586d2baa1
Susant1997 <88119172+Susant1997@users.noreply.github.com>
2021-12-01 20:06:41 +1300

    Add files via upload

---
+++ Slot Game/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UWPApp2
+{
+    class Utils
+    {
+
+        public static void load(int Wheel, Image imageWheel)
+        {
+            if (Wheel == 1) imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/lose.png",
+                                                                    UriKind.RelativeOrAbsolute));
+            else if (Wheel == 2) imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/spade.png",
+                                                                       UriKind.RelativeOrAbsolute));
+            else if (Wheel == 3) imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/club.png",
+                                                                       UriKind.RelativeOrAbsolute));
+            else if (Wheel == 4) imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/heart.png",
+                                                                       UriKind.RelativeOrAbsolute));
+            else if (Wheel == 5) imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/diamond.png",
+                                                                       UriKind.RelativeOrAbsolute));
+            else imageWheel.Source = new BitmapImage(new Uri("ms-appx:///Assets/win.png",
+                                                              UriKind.RelativeOrAbsolute));
+
+        }
+        public static void tag(Boolean wheelClicked, Image imageWheel)
+        {
+            if (wheelClicked == false)
+            {
+                wheelClicked = true;
+                imageWheel.Opacity = 0.5f;
+            }
+            else
+            {
+                wheelClicked = false;
+                imageWheel.Opacity = 1f;
+            }
+        }
+    }
+}
